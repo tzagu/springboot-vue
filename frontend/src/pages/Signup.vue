7629c9f6058c0d40da195af391bd7f5100d1dc6f
vizuamatix <s.yapa@ucdconnect.ie>
2020-08-12 12:56:10 +0530

    frontend helloworld removed

---
+++ frontend/src/pages/Signup.vue
@@ -0,0 +1,24 @@
+<template>
+<v-form>
+    <v-container>
+        <v-row>
+            <v-col cols="12" sm="6">
+          <v-text-field
+            label="Email Address"
+            single-line
+          ></v-text-field>
+        </v-col>
+        </v-row>
+    </v-container>
+</v-form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
